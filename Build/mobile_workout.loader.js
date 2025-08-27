1582532767984973518dd461c9774904312c2a51
Almaskhan Baimyshev <xkhannx@gmail.com>
2025-08-28 01:30:41 +0500

    Add debug

--- Build/mobile_workout.loader.js
+++ Build/mobile_workout.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 99028053,
+    wasmFileSize: 99028165,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
