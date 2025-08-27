1b1bbd2cf428d7d663fa8760cde773fc3b41e829
Almaskhan Baimyshev <xkhannx@gmail.com>
2025-08-28 00:44:49 +0500

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "flection.ai-Mobile Workout-0.1.0";
+const contentToCache = [
+    "Build/mobile_workout.loader.js",
+    "Build/mobile_workout.framework.js",
+    "Build/mobile_workout.data",
+    "Build/mobile_workout.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
